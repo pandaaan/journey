21a12de3309f80b132b7d7b48e4973d9557dc1c2
pandaaan <nhudan06@gmail.com>
2020-02-20 13:23:15 +0100

    Updates

---
+++ precache-manifest.ad0a6242f55c20387f6b400ed247f90a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f401a9d22fda65e98e83e531e216db9e",
+    "url": "/journey/index.html"
+  },
+  {
+    "revision": "0af640442dbfc73c2eb7",
+    "url": "/journey/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b6027bc3d5d53ebe6640",
+    "url": "/journey/static/css/main.c6a95d2b.chunk.css"
+  },
+  {
+    "revision": "0af640442dbfc73c2eb7",
+    "url": "/journey/static/js/2.1bdbd498.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/journey/static/js/2.1bdbd498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6027bc3d5d53ebe6640",
+    "url": "/journey/static/js/main.5aa586c4.chunk.js"
+  },
+  {
+    "revision": "6c98debe8d7f7112481a",
+    "url": "/journey/static/js/runtime-main.178027d1.js"
+  }
+]);
